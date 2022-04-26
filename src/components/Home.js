a58ed8eb7f13dfb63e16666a3b9e1a821624d0e0
Kaitlyn Greve <kgreve14@gmail.com>
2022-04-26 11:10:07 -0400

    completed tests

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,13 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  const h1 = `${name} is a Web Developer from ${city}`
+  return (
+    <div id="home">
+      <h1 style={{ color: "firebrick"}}>{h1}</h1>
+    
+    </div>
+  );
 }
 
 export default Home;
